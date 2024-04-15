9965c6429c7eda76b2dcadf250dfcd359564e124
Korawit <korawit.sit@mail.kmutt.ac.th>
2024-04-15 21:51:14 +0700

    [UPDATE]...

--- services/restful/Accountapi.js
+++ services/restful/Accountapi.js
@@ -18,13 +18,13 @@ export async function updateDisplayName(displayName) {
   console.log(displayName);
   let response = await axios
     .post(
-      // `${import.meta.env.VITE_API_ENV}/auth`,
-      `http://localhost:8080/local/api/edit-account?dname=${displayName}`,
+      `${import.meta.env.VITE_API_ENV}/edit-account?dname=${displayName}`,
+      // `http://localhost:8080/local/api/edit-account?dname=${displayName}`,
       {
         headers: {
           'Access-Control-Allow-Origin': "*",
           "Content-Type": "application/json",
-          Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJwcm9maWxlVVJMIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jTENYVGhxa1lodV9jREZ5NDNVZGJUMHdScllrRnZjcHJBd3NwLTFFVWZINW0yWE9RPXM5Ni1jIiwic3ViIjoiYmtvc3MxNzE1Mjk0OEBnbWFpbC5jb20iLCJyb2xlIjpbIlBhcnRpY2lwYW50cyJdLCJkaXNwbGF5TmFtZSI6IktvcmF3aXQgV2F0dGEuIiwiZXhwIjoxNzEzMTE2OTM3LCJpYXQiOjE3MTMxMTUxMzd9.O5IuoayPfws-ZXv4MnqHSVZcSTBVNkMhD7XG-n87LNNKrxjuue4gQzzf5iYRGmq_y9431NmE2QMFL2UDvJP35g`,
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
       }
     )

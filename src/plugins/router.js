4d4b99ed25f8976cce022fb06b3ebc906d7f17c8
Korawit <korawit.sit@mail.kmutt.ac.th>
2024-04-27 18:15:35 +0700

    [UPDATE]Remove Participants view

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -6,7 +6,6 @@ const routes = [
     {path:'/myEvents/:email',name:'myEvents',component:()=>import(/* webpackChunkName: "my events" */ '@/pages/MyEvents.vue')},
     {path:'/profile',name:'dashboard',component:()=>import( /* webpackChunkName: "Dashboard" */'@/views/Dashboard.vue')},
     {path:'/events',name:'events',component:()=>import(/* webpackChunkName: "Event" */'@/views/Events.vue')},
-    {path:'/participants',name:'participants',component:()=>import(/* webpackChunkName: "Participants" */'@/views/Participants.vue')},
     {path:'/create',name:'create',component:()=>import(/* webpackChunkName: "Create" */'@/pages/CreateEvent.vue')},
     {path:'/editEvent/:id',name:'editEvent',component:()=>import(/* webpackChunkName: "Edit" */'@/pages/EditEvent.vue')},
     {path:'/eventDetail/:id',name:'eventDetail',component:()=>import(/* webpackChunkName: "Edit" */'@/pages/EventDetail_card.vue')},
